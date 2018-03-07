19f4f8b6dc57a3495e82038044dcafa51344c36e
kostadinterziyski92 <kotttse@gmail.com>
2018-03-07 16:01:08 +0200

    added animal farm

--- 02.C# Fundamentals/02.OOPBasics/03.Encapsulation/02.ClassBoxDataValidation/StartUp.cs
+++ 02.C# Fundamentals/02.OOPBasics/03.Encapsulation/02.ClassBoxDataValidation/StartUp.cs
@@ -10,8 +10,8 @@
                 double length = double.Parse(Console.ReadLine());
                 double width = double.Parse(Console.ReadLine());
                 double height = double.Parse(Console.ReadLine());
-                var box = new Box(length, width, height);
 
+                var box = new Box(length, width, height);
                 Console.WriteLine(box.ToString());
             }
             catch (Exception ex)
