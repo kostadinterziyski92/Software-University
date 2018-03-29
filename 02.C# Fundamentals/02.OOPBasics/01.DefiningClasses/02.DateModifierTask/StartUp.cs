dcf0ad5dcecabe266ba778c1a0eacba224c69039
kostadinterziyski92 <kotttse@gmail.com>
2018-03-29 17:46:16 +0300

    finally fixed and added dateModifier Task

---
+++ 02.C# Fundamentals/02.OOPBasics/01.DefiningClasses/02.DateModifierTask/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.DateModifierTask
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var date1 = Console.ReadLine();
+            var date2 = Console.ReadLine();
+
+            DateModifier modifier = new DateModifier();
+            modifier.DateDifference(date1, date2);
+            Console.WriteLine(modifier.Difference);
+        }
+    }
+}
